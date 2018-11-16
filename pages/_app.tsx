54bc4d6b361d3bb4b031206ba7f1782b97a7067f
Jeroen Reumkens <hello@jeroenreumkens.nl>
2018-11-16 13:08:14 +0100

    Added GA code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -26,6 +26,21 @@ export default class MyApp extends App {
             href="https://fonts.googleapis.com/css?family=Source+Code+Pro:400,700"
             rel="stylesheet"
           />
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-29300344-2"
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){window.dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'UA-29300344-2');
+              `
+            }}
+          />
         </Head>
         <ThemeProvider theme={theme}>
           <React.Fragment>
